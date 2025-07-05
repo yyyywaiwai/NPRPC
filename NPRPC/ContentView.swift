86064c1b2804d2e43285d9544a92d4671dd664d9
yyyywaiwai <y.yajima1205@icloud.com>
2025-07-05 22:22:38 +0900

    Initial Commit

---
+++ NPRPC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NPRPC
+//
+//  Created by yyyywaiwai on 2025/07/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NPRPC/NPRPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  NPRPCApp.swift
+//  NPRPC
+//
+//  Created by yyyywaiwai on 2025/07/05.
+//
+
+import SwiftUI
+
+@main
+struct NPRPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
